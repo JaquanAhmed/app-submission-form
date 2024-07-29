ebc357830d8cd6e926fc093116b352467d170de2
Jaquan Ahmed <JAhmed@mrc.local>
2024-07-29 13:09:07 +0100

    cat commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template;
+
+app = Flask(__name__);
+
+@app.route('/')
+
+def cat():
+    return render_template('cat.html')
+
+if __name__ == '__main__':
+    app.run(debug = "True")
