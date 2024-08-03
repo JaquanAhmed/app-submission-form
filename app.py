6a9e6bd0825391ecf7879953df0fc330b9072ea4
JaquanAhmed <jaq-ahmed@hotmail.co.uk>
2024-08-03 15:01:03 +0100

    form created

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__);
 @app.route('/')
 
 def cat():
-    return render_template('cat.html')
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run(debug = "True")
