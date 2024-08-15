1a13c362533192ab9e608a52444725a0d20a1684
JaquanAhmed <jaq-ahmed@hotmail.co.uk>
2024-08-15 07:48:48 +0100

    added home page (not finsihed)

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from flask import Flask, render_template;
 app = Flask(__name__);
 
 @app.route('/')
-
-def cat():
+def home():
+    return render_template('home.html')
+@app.route('/form')
+def about():
     return render_template('form.html')
 
 if __name__ == '__main__':
