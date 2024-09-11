7c5b5eee4cec432e226c15bcbbbaa3a8e194ff69
Jaquan Ahmed <JAhmed@mrc.local>
2024-09-11 17:45:12 +0100

    fixed some buttons on form

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 from flask import Flask, render_template, redirect, request, url_for
-from flask_login import (
-    LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
-)
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
 from oauthlib.oauth2 import WebApplicationClient
 import requests
 import json
@@ -128,7 +122,6 @@ def home():
             return render_template('home.html')
         else:
             return redirect(url_for("login"))
-    
 
 @app.route('/form')
 def form():
